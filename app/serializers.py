85286ee43ab2de781f3e99edfe13194aed3d857a
wojiaomigz <migzwebhost@gmail.com>
2015-12-09 16:33:22 +0800

    Removed unnecessary viewsets and serializers in app.serializers

--- app/serializers.py
+++ app/serializers.py
@@ -1,9 +1,6 @@
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 
-from rest_framework import permissions, serializers, viewsets
-
-from oauth2_provider.ext.rest_framework import TokenHasReadWriteScope
-from oauth2_provider.ext.rest_framework import TokenHasScope
+from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -13,21 +10,3 @@ class UserSerializer(serializers.ModelSerializer):
                   'first_name', 'last_name')
         read_only_fields = 'id',
         extra_kwargs = {'password': {'write_only': True}}
-
-
-class GroupSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Group
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    permission_classes = permissions.IsAuthenticated, TokenHasReadWriteScope
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class GroupViewSet(viewsets.ModelViewSet):
-    permission_classes = permissions.IsAuthenticated, TokenHasScope
-    required_scopes = 'groups',
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
