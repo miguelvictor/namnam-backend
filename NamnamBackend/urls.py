6b77acaf0d8ed3bb130fa73196f73da1dc5efdaf
Miguel Victor Remulta <migzwebhost@gmail.com>
2015-12-22 15:11:42 +0800

    Removed unused urls in rest-auth

--- NamnamBackend/urls.py
+++ NamnamBackend/urls.py
@@ -14,8 +14,6 @@ router.register(r'ingredient-types', viewsets.IngredientTypeViewSet)
 urlpatterns = [
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('app.urls', namespace='app')),
-    url(r'^api/auth/', include('rest_framework.urls',
-        namespace='rest_framework')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^oauth/', include('oauth2_provider.urls',
         namespace='oauth2_provider')),
