fb4bb1535261a637794cd145cf0c33eae7d24d43
ozknightwalker <ozknightwalker62@gmail.com>
2016-01-23 17:10:25 +0800

    created app to generate email verification

--- NamnamBackend/settings.py
+++ NamnamBackend/settings.py
@@ -120,6 +120,15 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+# SMTP GMAIL
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'cookstogosystem@gmail.com'
+EMAIL_HOST_PASSWORD = 'cooksadmin'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 # OAUTH
 
 OAUTH2_PROVIDER = {
